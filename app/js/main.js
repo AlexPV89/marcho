22e68f53891ef164226c7b0ea5258bdf11b4f17c
AlexPV89 <pustryakov89@mail.ru>
2022-02-28 19:14:50 +0300

    added video fashion

--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,14 @@ $(function(){
 
     
 })
+
+    $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 0,
+            showinfo : 0
+        },
+        vimeo : {
+            color : 'f00'
+        }
+    });
+   
