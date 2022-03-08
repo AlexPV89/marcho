b67f36c81a953e7866ead1f525e14a6fa89543dc
AlexPV89 <pustryakov89@mail.ru>
2022-03-08 15:35:52 +0300

    added promo and partners

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
     $('.top-slider__inner').slick({
         dots: true,
@@ -13,11 +15,7 @@ $(function(){
         ratedFill: '#ffc35b',
         readOnly: true,
       });
-
-    
-})
-
-    $('[data-fancybox]').fancybox({
+      $('[data-fancybox]').fancybox({
         youtube : {
             controls : 0,
             showinfo : 0
@@ -25,6 +23,53 @@ $(function(){
         vimeo : {
             color : 'f00'
         }
-    });
+        
+    } );
+   const deadLine = document.querySelector('.promo__clock-item').getAttribute('data-time')
+
+function getZero (time) {
+  return  (time >= 0 && time < 10) ? `0${time}` : time; 
+}
+function getTime (endTime) {
+ const timer = Date.parse(endTime) - Date.parse(new Date()),
+       days = Math.floor(timer / (1000 * 60 * 60 * 24)),
+       hours = Math.floor((timer / (1000 * 60 * 60)) % 24),
+       minutes = Math.floor((timer / (1000 * 60) )% 60),
+       seconds = Math.floor((timer / (1000) )% 60)
+  return {
+    t: timer,
+    days : getZero(days),
+    hours: getZero(hours),
+    minutes: getZero(minutes),
+    seconds: getZero(seconds)
+  }
+}
+ 
+function setClock (selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('.promo__days'),
+          hours = timer.querySelector('.promo__hours'),
+          minutes = timer.querySelector('.promo__minutes'),
+          seconds = timer.querySelector('.promo__seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+          updateClock()
+    function updateClock () {
+      let t = getTime(deadLine);
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+      if(t.timer <= 0){
+        clearInterval(timeInterval)
+      }
+    }
+   
+}
+setClock('.promo__clock', deadLine)
+})
+
+   
+   
    
 
+   
