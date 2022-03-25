18ad357c7977ee59bafffba3b894d247db9c251b
AlexPV89 <pustryakov89@mail.ru>
2022-03-25 20:02:18 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,56 @@
 
 
 $(function(){
+
+  const btn = document.querySelectorAll('.shop-content__filter-btn');
+  let product = document.querySelectorAll('.product-item')
+
+  btn.forEach((el,i) => {
+    
+  el.addEventListener('click',function () {
+    
+      btn.forEach(el => el.classList.remove('shop-content__filter-btn--active'))
+      
+      if(el === btn[i]){
+        el.classList.toggle('shop-content__filter-btn--active')
+      }
+   
+
+      if(el.classList.contains('button-grid')){
+       
+       product.forEach(el => {
+        el.classList.remove('product-item--list')
+         el.classList.add('product-item--grid')
+       })
+        
+        
+      }
+      if(el.classList.contains('button-list')){
+       product.forEach(el => {
+         el.classList.remove('product-item--grid')
+        el.classList.add('product-item--list')
+      })}
+    
+
+  })
+  })
+
+  $('.select-style').styler()
+  let from = document.querySelector('.filter-price__from'),
+            to = document.querySelector('.filter-price__to')
+
+    $(".filter-price__input").ionRangeSlider({
+      type:"double",
+      prifix: '$',
+      onStart: function (data) {
+        from.innerHTML = `$${data.from}`;
+        to.innerHTML = `$${data.to}`;
+    },
+      onChange: function (data) {
+        from.innerHTML = `$${data.from}`;
+        to.innerHTML = `$${data.to}`;
+      }
+    });
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
@@ -34,8 +84,8 @@ function getTime (endTime) {
  const timer = Date.parse(endTime) - Date.parse(new Date()),
        days = Math.floor(timer / (1000 * 60 * 60 * 24)),
        hours = Math.floor((timer / (1000 * 60 * 60)) % 24),
-       minutes = Math.floor((timer / (1000 * 60) )% 60),
-       seconds = Math.floor((timer / (1000) )% 60)
+       minutes = Math.floor((timer / (1000 * 60) ) % 60),
+       seconds = Math.floor((timer / (1000) ) % 60)
   return {
     t: timer,
     days : getZero(days),
